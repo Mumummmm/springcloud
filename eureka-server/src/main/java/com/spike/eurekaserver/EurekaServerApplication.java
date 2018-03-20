1b8dd890d03ca0f91544f1434b7e0e760c0b632c
spike <526915701@qq.com>
2018-03-20 14:16:20 +0800

    init

--- eureka-server/src/main/java/com/spike/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/spike/eurekaserver/EurekaServerApplication.java
@@ -2,6 +2,7 @@ package com.spike.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 启动类
